0516107f756798562f868d7ce7c23c07d406c1c8
AlexCrisTin <153697285+AlexCrisTin@users.noreply.github.com>
2024-03-25 20:21:52 +0700

    Add files via upload

---
+++ src/MainSceneController.java
@@ -0,0 +1,199 @@
+
+import java.io.File;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class MainSceneController {
+
+    
+                     
+    
+private Stage stage;
+private Scene scene;
+private Parent root;
+
+                //Login and Logout method
+                
+
+@FXML
+public TextField nameTextField;
+public void Login(ActionEvent event) throws IOException{
+    
+    /*String username = nameTextField.getText();
+
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
+
+    root = loader.load();
+    Login login = loader.getController();
+    login.displayName(username);
+    */
+    root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+    @FXML
+    private AnchorPane scenePane;
+
+public void Logout(ActionEvent event) {
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Logout?");
+    alert.setHeaderText("Are you sure?");
+
+    if(alert.showAndWait().get() == ButtonType.OK){
+        stage = (Stage) scenePane.getScene().getWindow();
+     
+        stage.close();
+    }
+}
+
+                    // Button Click
+
+public void btnExitClicked(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+public void btnCreateClicked(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("Create.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+public void Create(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("Create1.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+public void Delete(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("Delete.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+public void Borrow_ReturnBook(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("Borrow_return.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+public void Borrow(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("Borrow_return1.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+public void CheckBook(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("CheckBook.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+                                 // Find book
+
+private TextField findBook;
+private Button Find;
+
+String find;
+public void Find(ActionEvent event){
+    
+    
+
+}
+                    //Book
+
+public void btnViewClicked(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("B1.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+public void btnView1Clicked(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("B2.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+public void btnView2Clicked(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("B3.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+public void btnView3Clicked(ActionEvent event) throws IOException{
+    root = FXMLLoader.load(getClass().getResource("B4.fxml"));
+    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+                                //Pick photo
+@FXML
+private ImageView imgView;
+
+
+    public void Pick(ActionEvent event) throws IOException{
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setInitialDirectory(new File("C:\\"));
+    fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JPEG image","*.jpg"),
+        new FileChooser.ExtensionFilter("PNG image", "*.png"), new FileChooser.ExtensionFilter("All images", "*.jpg","*.png"));
+        File selectFile = fileChooser.showOpenDialog(null);
+      
+        Image image = new Image(selectFile.getPath());
+        imgView.setImage(image);
+    
+}
+
+}
+            
