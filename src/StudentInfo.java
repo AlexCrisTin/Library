064cecb836522e65b08c22d89c7ad0bb97ff63ca
AlexCrisTin <nholaian2222@gmail.com>
2024-04-07 17:20:52 +0700

    check

--- src/StudentInfo.java
+++ src/StudentInfo.java
@@ -50,66 +50,76 @@ public class StudentInfo {
     @FXML
     private CheckBox rule;
     @FXML
-    void rent(ActionEvent event) throws IOException {
-        Alert alert;
-         if (rule.isSelected()) {
+void rent(ActionEvent event) throws IOException {
+    Alert alert;
+    if (rule.isSelected()) {
         String[] StudentInfo = new String[4];
         StudentInfo[0] = namestudent.getText();
         StudentInfo[1] = MSSV.getText();
         StudentInfo[2] = day.getText();
         StudentInfo[3] = daypay.getText();
+
+        // Check if any field is empty
+        for (String info : StudentInfo) {
+            if (info == null || info.trim().isEmpty()) {
+                alert = new Alert(AlertType.WARNING);
+                alert.setTitle("Missing Information");
+                alert.setHeaderText("Làm ơn điền thông tin");
+                alert.showAndWait();
+                return;  // Exit the method
+            }
+        }
+
         alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Rent Complete");
         alert.setHeaderText("Rent Complete");
         
         saveToFile(StudentInfo);
-        }else{
-            alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("MY TERRITORY, MY RULES!");
-            alert.setHeaderText("Bạn chưa đọc luật lệ");
-            if(alert.showAndWait().get() == ButtonType.OK){
-            
-                Parent NewStudentInterface = FXMLLoader.load(getClass().getResource("/FXML/info.fxml"));
-                Scene NewStudentScene = new Scene(NewStudentInterface); 
-                Stage window = (Stage)((Button) event.getSource()).getScene().getWindow(); 
-                window.setScene(NewStudentScene);
-                window.show();
-                }
-        }
+    } else {
+        alert = new Alert(AlertType.WARNING);
+        alert.setTitle("MY TERRITORY, MY RULES!");
+        alert.setHeaderText("Bạn chưa đọc luật lệ");
         if(alert.showAndWait().get() == ButtonType.OK){
             
+            Parent NewStudentInterface = FXMLLoader.load(getClass().getResource("/FXML/info.fxml"));
+            Scene NewStudentScene = new Scene(NewStudentInterface); 
+            Stage window = (Stage)((Button) event.getSource()).getScene().getWindow(); 
+            window.setScene(NewStudentScene);
+            window.show();
+        }
+    }
+    if(alert.showAndWait().get() == ButtonType.OK){
+        
         Parent NewStudentInterface = FXMLLoader.load(getClass().getResource("/FXML/MainScene.fxml"));
         Scene NewStudentScene = new Scene(NewStudentInterface); 
         Stage window = (Stage)((Button) event.getSource()).getScene().getWindow(); 
         window.setScene(NewStudentScene);
         window.show();
-        }
-    
-    
+    }
 }
-    private void saveToFile(String[] StudentInfo) {
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("StudentData.dat", true)))) {
-            for (String info : StudentInfo) {
-                if (info != null) {
-                    String binaryString = stringToBinary(info);
-                    writer.write(binaryString + ","); 
-                }
+private void saveToFile(String[] StudentInfo) {
+    try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("StudentData.dat", true)))) {
+        for (String info : StudentInfo) {
+            if (info != null) {
+                String binaryString = stringToBinary(info);
+                writer.write(binaryString + ","); 
             }
-            writer.newLine();
-            writer.flush(); 
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        writer.newLine();
+        writer.flush(); 
+    } catch (IOException e) {
+        e.printStackTrace();
     }
-    private String stringToBinary(String str) {
-        StringBuilder binary = new StringBuilder();
-        for (char c : str.toCharArray()) {
-            String binaryChar = Integer.toBinaryString(c);
-            binary.append(binaryChar).append(" ");
-        }
-        return binary.toString();
+}
+private String stringToBinary(String str) {
+    StringBuilder binary = new StringBuilder();
+    for (char c : str.toCharArray()) {
+        String binaryChar = Integer.toBinaryString(c);
+        binary.append(binaryChar).append(" ");
+    }
+    return binary.toString();
 
-    }
+}
     
     
 }
