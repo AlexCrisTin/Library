86cb03def1f5c4b74ff03ea4e47c3f3dd29c9575
AlexCrisTin <nholaian2222@gmail.com>
2024-04-07 22:31:34 +0700

    d

---
+++ src/StudentDetail.java
@@ -0,0 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class StudentDetail {
+
+    @FXML
+    private Label daily;
+
+    @FXML
+    private Label dailypay;
+
+    @FXML
+    private Label id;
+
+    @FXML
+    private Label name;
+
+    @FXML
+    private Label namebook;
+
+    @FXML
+    void information(ActionEvent event) {
+
+    }
+
+}
