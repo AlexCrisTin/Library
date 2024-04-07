9f7e7c4f93aa91ae75ea83e16d317f138f286810
AlexCrisTin <nholaian2222@gmail.com>
2024-04-07 16:50:52 +0700

    icon

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -17,7 +17,8 @@ public class App extends Application {
         try {
             root = FXMLLoader.load(getClass().getResource("/FXML/MainScene.fxml"));
             Scene scene = new Scene(root);
-
+            Image image = new Image("/icon/2232688.png");
+            primaryStage.getIcons().add(image);
             primaryStage.setTitle("Library");
             primaryStage.setScene(scene);
             primaryStage.show();
