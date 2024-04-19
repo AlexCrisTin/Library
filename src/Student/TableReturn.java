a154b34b9ece906020f9937ffeb528d25eb4cbfe
AlexCrisTin <nholaian2222@gmail.com>
2024-04-19 15:42:04 +0700

    add find mssv

--- src/Student/TableReturn.java
+++ src/Student/TableReturn.java
@@ -1,6 +1,8 @@
 package Student;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +14,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import java.io.BufferedReader;
@@ -21,6 +24,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+
 
 
     public class TableReturn {
@@ -39,6 +44,11 @@ import java.util.List;
         private TableColumn<StudentInformation, String> dayPayColumn;
         @FXML
         private TableColumn<StudentInformation, String> bookColumn;
+        @FXML
+        private TextField search;
+
+        
+
         public void initialize() {
             nameColumn.setCellValueFactory(new PropertyValueFactory<>("namestudent"));
             mssvColumn.setCellValueFactory(new PropertyValueFactory<>("MSSV"));
@@ -46,6 +56,24 @@ import java.util.List;
             dayPayColumn.setCellValueFactory(new PropertyValueFactory<>("daypay"));
             bookColumn.setCellValueFactory(new PropertyValueFactory<>("book"));
             tableView.setItems(getStudents());
+            FilteredList<StudentInformation> filteredList = new FilteredList<>(getStudents(), b -> true);{
+                search.textProperty().addListener((observable, oldValue, newValue) -> {
+                    filteredList.setPredicate(mssv -> {
+                        if (newValue == null || newValue.isEmpty()) {
+                            return true;
+                        }
+                
+                        String lowerCaseFilter = newValue.toLowerCase();
+                
+                        if (mssv.getMSSV().toLowerCase().contains(lowerCaseFilter)) {
+                            return true;
+                        }
+                        return false;
+                    });
+                });}
+                SortedList<StudentInformation> sortedData = new SortedList<>(filteredList);
+                sortedData.comparatorProperty().bind(tableView.comparatorProperty());
+                tableView.setItems(sortedData);
         }
     
         private ObservableList<StudentInformation> getStudents() {
